2ee4708227b323cda1df3006ac1b162c5d3a01c1
charan22640 <balinasaicharan23@gmail.com>
2025-05-13 06:15:33 +0530

    railway deployment

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'nozomi.proxy.rlwy.net',
   user: 'root',
-  password: process.env.DB_PASSWORD, // We'll set this in Railway's environment variables
+  password: process.env.DB_PASSWORD,
   database: 'railway',
   port: 32331,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   }
 });
 
